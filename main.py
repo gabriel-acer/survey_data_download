71833ec0d4e23cf3d87b5954fde252e5f70bbe03
Gabriel Yin <58231939+gabriel-acer@users.noreply.github.com>
2019-12-24 16:07:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import numpy as np
 from google.cloud import storage, bigquery
 from bluelabs import BluelabsDataLoader, BluelabsDataAggregator
 from survey_monkey import SurveyMonkeyDataLoader
+from combine_survey_data import SurveyDataCombiner
 import warnings
 warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     #SurveyMonkeyDataLoader().download_from_big_query()
     #BluelabsDataLoader().download_from_big_query()
-    bl = BluelabsDataAggregator().clean().voter_age_zip().decode_cols().save()
-    sm = SurveyMonkeyDataLoader().download_from_big_query().clean().decode().save()
+    bl = BluelabsDataAggregator().run()
+    sm = SurveyMonkeyDataLoader().run()
+    final = SurveyDataCombiner()
+    
